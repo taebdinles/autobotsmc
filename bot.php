5a2866a79c1bff0154aa374b711027a27ec075f3
taebdinles <43269163+taebdinles@users.noreply.github.com>
2018-09-14 21:42:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,7 +15,12 @@ if (!is_null($events['events'])) {
   foreach ($events['events'] as $event) {
     // Reply only when message sent is in 'text' format
     if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-    	 curl_setopt($ch, CURLOPT_URL, "http://101.109.246.31/dotsmc/pages/testaddtodb.php");
+	    
+	  $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://101.109.246.31/dotsmc/pages/testaddtodb.php");
+$productivity = curl_exec($ch);  
+	    
+    	 
  	//header( "location: http://101.109.246.31/dotsmc/pages/testaddtodb.php" );
 	
     
