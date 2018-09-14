034bb7cb5ad09ea440b043224dea61f404f45854
taebdinles <43269163+taebdinles@users.noreply.github.com>
2018-09-14 19:31:21 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,5 @@
 <?php
 $access_token = 'tCFfog05pnNUDnqL2VZxR6NMUxeSrWq/Boi0+E8spF4A4ofr3rlSxUQAF1qbSWEWmZM7mG4blFkmD6j4JwmpW5YO+pNn9YDyGyXWG156fdHl8uO5ZEr15glAEWRbzR84wEfA0MFGTVPE1lkO8/CEmQdB04t89/1O/w1cDnyilFU=';
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:W8B1EissNtwSwyk';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
@@ -11,8 +9,6 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
