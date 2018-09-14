d81926170cdc2df7452a07a324ce6ff57f4c18cc
taebdinles <43269163+taebdinles@users.noreply.github.com>
2018-09-14 21:14:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,10 +15,10 @@ if (!is_null($events['events'])) {
   foreach ($events['events'] as $event) {
     // Reply only when message sent is in 'text' format
     if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-    <?
+    
  	header( "location: http://101.109.246.31/dotsmc/pages/testaddtodb.php" );
 	
-    ?>
+    
        // Get text sen
 	$replyText = 'test';   
      
