e728b7d0791b175a1fd2df863579a8a1a9dab059
taebdinles <43269163+taebdinles@users.noreply.github.com>
2019-05-07 17:08:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,7 +33,8 @@ if (!is_null($events['events'])) {
         $replyText = '1.หากต้องการตรวจสอบข้อมูลเขหมายJให้พิม!!ตามด้วยหมายเลขวงจร<br/>2.หากต้องการตรวจSessionให้พิม??ตามด้วยหมายเลขวงจร\n\n3.Autoconfig(ZTE)ให้พิม@@ตามด้วยเลขวงจร ';
       } else if ($message == 'sm') {
 	     
-	
+	  $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://203.114.98.244/index.php?r=WebService/sr");
         	
        		$replyText = 'test';
         
